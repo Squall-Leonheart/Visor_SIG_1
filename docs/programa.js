060caecb2af11f1a402c81a61fd7daae4eda8d32
Giovanny González Prieto <gp.giovanny@gmail.com>
2025-08-05 14:49:09 -0500

    V4.0

--- docs/programa.js
+++ docs/programa.js
@@ -3,3 +3,4 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+var marker = L.marker([4.628168497532548, -74.06596444204271]).addTo(map);
